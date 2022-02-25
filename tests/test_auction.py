1dbfd840cef3bd926e2218665dcffe9097f3f1ba
KRISHNA CHAITANYA <59798012+Krishduttm@users.noreply.github.com>
2022-02-25 19:33:52 +0530

    Delete test_auction.py

--- tests/test_auction.py
+++
@@ -1,9 +0,0 @@
-
-
-from scripts.deploy_auction import get_account
-from scripts.deploy_auction import get_account
-from brownie
-
-def test_can_get_highestBidder():
-    account = get_account()
-    
